a1ed84fa904d6aac5c3028b4f73f246f6cbeea8d
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-08-01 15:46:10 +0200

    Setup my `Header` component

---
+++ src/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <header class="sticky z-50 top-0">
+    <nav class="bg-primary-medium border-primary-dark">
+      <div
+        class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
+      >
+        <router-link
+          to="/"
+          class="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <img src="../assets/logo.png" class="h-8" alt="Flowbite Logo" />
+          <span
+            class="self-center text-2xl font-semibold whitespace-nowrap text-primary-light"
+            >Emercom</span
+          >
+        </router-link>
+        <button @click="toggleMenu" class="md:hidden">
+          <!-- Menu icon -->
+        </button>
+        <div
+          :class="{ hidden: !isMenuOpen }"
+          class="w-full md:block md:w-auto"
+          id="navbar-default"
+        >
+          <ul
+            class="flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white"
+          >
+            <li>
+              <router-link
+                to="/"
+                class="block py-2 px-3 text-primary-dark rounded hover:bg-primary-accent1 md:hover:bg-transparent md:border-0 md:hover:text-primary-accent2 md:p-0"
+                >Home</router-link
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      isMenuOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
+  },
+};
+</script>
