f0702b329523f815f10d2c6eb53378b8dd840416
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-08-02 10:29:16 +0200

    Added `Home` route and component to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../components/Home.vue'
 import ProductList from '../components/ProductList.vue';
 import ProductDetails from '../components/ProductDetails.vue';
 
 const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
     {
         path: '/',
         name: 'ProductList',
