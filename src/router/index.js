b341c50e6543c35c0cec17af134dd27d7309a36f
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-08-05 15:13:30 +0200

    Added JSDoc to index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,41 @@ import Home from '../components/Home.vue'
 import ProductList from '../components/ProductList.vue';
 import ProductDetails from '../components/ProductDetails.vue';
 
+/**
+ * Array of route objects.
+ * Each route object defines a route for the application.
+ * 
+ * @type {Array<Object>}
+ * @property {string} path - The URL path for the route.
+ * @property {string} [name] - The name of the route.
+ * @property {Object} component - The component to be rendered for the route.
+ */
 const routes = [
     {
         path: '/',
         redirect: '/products'
     },
+    /**
+     * Route for the product list page.
+     * 
+     * @type {Object}
+     * @property {string} path - The URL path for the route.
+     * @property {string} name - The name of the route.
+     * @property {Object} component - The component to be rendered for the route.
+     */
     {
         path: '/products',
         name: 'ProductList',
         component: ProductList
     },
+    /**
+     * Route for the product details page.
+     * 
+     * @type {Object}
+     * @property {string} path - The URL path for the route.
+     * @property {string} name - The name of the route.
+     * @property {Object} component - The component to be rendered for the route.
+     */
     {
         path: '/product/:id',
         name: 'ProductDetails',
@@ -20,6 +45,12 @@ const routes = [
     }
 ];
 
+/**
+ * Creates a router instance.
+ * 
+ * @function
+ * @returns {Object} - Vue Router instance.
+ */
 const router = createRouter({
     history: createWebHistory(),
     routes
